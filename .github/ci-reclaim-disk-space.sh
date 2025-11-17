31557791bc77cff399b395de1634c547f17d9b0a
Eric Deandrea <eric.deandrea@gmail.com>
2025-11-17 09:09:38 -0500

    Fix disk space issue
    Signed-off-by: Eric Deandrea <eric.deandrea@gmail.com>

--- .github/ci-reclaim-disk-space.sh
+++ .github/ci-reclaim-disk-space.sh
@@ -2,26 +2,16 @@
 
 # Reclaim disk space, otherwise we have too little free space at the start of a job
 time sudo docker image prune --all --force || true
-# That is 979M
 time sudo rm -rf /usr/share/dotnet || true
-# That is 1.7G
 time sudo rm -rf /usr/share/swift || true
-# Remove Android
 time sudo rm -rf /usr/local/lib/android || true
-# Remove Haskell
 time sudo rm -rf /opt/ghc || true
 time sudo rm -rf /usr/local/.ghcup || true
-# Remove pipx
 time sudo rm -rf /opt/pipx || true
-# Remove Rust
 time sudo rm -rf /usr/share/rust || true
-# Remove Go
 time sudo rm -rf /usr/local/go || true
-# Remove miniconda
 time sudo rm -rf /usr/share/miniconda || true
-# Remove powershell
 time sudo rm -rf /usr/local/share/powershell || true
-# Remove Google Cloud SDK
 time sudo rm -rf /usr/lib/google-cloud-sdk || true
 
 # Remove infrastructure things that are unused and take a lot of space
