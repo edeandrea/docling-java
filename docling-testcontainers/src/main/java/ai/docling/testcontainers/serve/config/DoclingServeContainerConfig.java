bad30c17d78f5695a3281cc23cc57cae0429f850
Eric Deandrea <eric.deandrea@ibm.com>
2026-02-24 13:20:32 -0500

    chore: Bump docling-serve container image to v1.13.0
    Signed-off-by: Eric Deandrea <eric.deandrea@ibm.com>

--- docling-testcontainers/src/main/java/ai/docling/testcontainers/serve/config/DoclingServeContainerConfig.java
+++ docling-testcontainers/src/main/java/ai/docling/testcontainers/serve/config/DoclingServeContainerConfig.java
@@ -28,7 +28,7 @@ public interface DoclingServeContainerConfig {
   /**
    * Represents the version identifier for the docling-serve container image.
    */
-  String DOCLING_IMAGE_VERSION = "v1.11.0";
+  String DOCLING_IMAGE_VERSION = "v1.13.0";
 
   /**
    * Default image name
